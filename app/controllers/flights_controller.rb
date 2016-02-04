9e35a1e4b2657231287702dbd2561de768893116
Patrick Mallee <patmallee@gmail.com>
2016-02-04 02:15:16 +0000

    Add search form to Flights#index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,9 @@
 class FlightsController < ApplicationController
   
   def index
+    @flight = Flight.new
+    @airports = Airport.all.map { |port| [port.code, port.id] }
+    @departures = Flight.select(:depart_time).distinct.map { |date| [date.depart_time.strftime("%b %d, %Y - %l:%M %P"), date] }
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ end
 
 20.times do
   day = 60*60*24
-  departure_time = Time.now+3.days
+  departure_time = Time.now+rand(10).days
   duration = (1..5).to_a.sample
   from_port = (1..20).to_a.sample
   to_port = (1..20).to_a.sample
