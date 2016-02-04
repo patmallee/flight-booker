53cb3fb6f3f43551181100d0095023f7c1c92e85
Patrick Mallee <patmallee@gmail.com>
2016-02-04 00:49:26 +0000

    Add user model and db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do
+  @code = ""
+  3.times do
+    @code += ('A'..'Z').to_a.sample
+  end
+  Airport.create(code: @code)
+end
