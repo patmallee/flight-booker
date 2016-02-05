d15dc9ff8d129f3c2acaa3ad1d740581f0a083c1
Patrick Mallee <patmallee@gmail.com>
2016-02-05 01:16:04 +0000

    Clean up params logic in search results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,15 +3,21 @@ class FlightsController < ApplicationController
   def index
     @flight = Flight.new
     @airports = Airport.all.map { |port| [port.code, port.id] }
-    @departures = Flight.select(:depart_time).distinct.map { |date| [date.depart_time.strftime("%b %d, %Y - %l:%M %P"), date] }
+    @departures = Flight.distinct.pluck(:depart_time).map { |date| [date.strftime("%b %d, %Y - %l:%M %P"), date] }
   
-    @search_results = Flight.where("depart_time >= ? AND from_airport_id = ? AND 
-                                    to_airport_id = ?", search_params[:depart_time], 
-                                    search_params[:from_airport_id], search_params[:to_airport_id])
-    @passenger_count = search_params[:passengers][:count]
+    unless params[:flight].nil?
+      @search_results = Flight.where("depart_time >= ? AND from_airport_id = ? AND 
+                                      to_airport_id = ?", search_params[:depart_time], 
+                                      search_params[:from_airport_id], search_params[:to_airport_id])
+      @passenger_count = search_params[:passengers][:count]
+    end
   end
   
   private
+  
+    def searched
+      @searched = true
+    end
   
     def search_params
       params.require(:flight).permit(:depart_time, :from_airport_id, :to_airport_id,
