629b0c854b3a987aee1eea8875010841ead46d48
Patrick Mallee <patmallee@gmail.com>
2016-02-06 02:36:00 +0000

    Create Passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206022909) do
+ActiveRecord::Schema.define(version: 20160206023520) do
 
   create_table "airports", force: :cascade do |t|
     t.string   "code"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20160206022909) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "duration"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
